433e9ca7112bfa091c877dae6070420a94b2afff
Ege Sevinç <egesevinc@windowslive.com>
2023-05-01 19:32:03 +0300

    fix

--- src/pages/marketplace.js
+++ src/pages/marketplace.js
@@ -11,22 +11,40 @@ const Marketplace = () => {
     { image: 'https://via.placeholder.com/150x150', price: 40 },
     { image: 'https://via.placeholder.com/150x150', price: 45 },
     { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
+    { image: 'https://via.placeholder.com/150x150', price: 50 },
   ];
+
+  // Add an empty product to fill the last row if necessary
+  if (products.length % 5 !== 0) {
+    products.push({ image: '', price: '' });
+  }
 
   return (
     <div style={{ padding: '24px' }}>
       <h1>Marketplace</h1>
       <Row gutter={[16, 16]}>
         {products.map((product, index) => (
-          <Col key={index} span={8}>
-            <Card
-              hoverable
-              cover={<img alt="product" src={product.image} />}
-            >
-              <Card.Meta title={`Product ${index + 1}`} description={`$${product.price}`} />
-            </Card>
+          <Col key={index} span={4}>
+            {product.image ? (
+              <Card
+                hoverable
+                cover={<img alt="product" src={product.image} />}
+              >
+                <Card.Meta
+                  title={`Product ${index + 1}`}
+                  description={`$${product.price}`}
+                />
+              </Card>
+            ) : null}
           </Col>
         ))}
+        {/* Add an empty card to fill the last row if necessary */}
+        {products.length % 5 !== 0 && <Col span={4} />}
       </Row>
     </div>
   );
