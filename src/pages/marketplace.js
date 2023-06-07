4cbda04d74907f99aa436ef4e46a5eecdea26b18
berkerugras <42471145+berkerugras@users.noreply.github.com>
2023-06-07 19:44:28 +0300

    marketplace'te ürünler gösteriliyor

--- src/pages/marketplace.js
+++ src/pages/marketplace.js
@@ -4,21 +4,6 @@ import axios from 'axios';
 
 const Marketplace = () => {
   const [products, setProduct] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:5000/posts/get-all-documents").then((res) => {
-      setProduct(res.data);
-      console.log(JSON.stringify(res.data));
-    });
-  }, []);
-
-  useEffect(() => {
-    console.log(products.length)
-
-  }, []);
-
-
-
   const productsa = [
     { image: 'https://via.placeholder.com/150x150', price: 10 },
     { image: 'https://via.placeholder.com/150x150', price: 15 },
@@ -37,26 +22,67 @@ const Marketplace = () => {
     { image: 'https://via.placeholder.com/150x150', price: 50 },
   ];
 
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts/get-all-documents").then((res) => {
+      setProduct(res.data);
+      console.log(JSON.stringify(res.data));
+    });
+  }, []);
+
   // Add an empty product to fill the last row if necessary
   if (products.length % 5 !== 0) {
     products.push({ image: '', price: '' });
   }
 
+  function checkIfProductsEmpty() {
+    if (products.length === 0) {
+      return <div style={{ padding: '24px' }}>
+        <h1>Marketplace</h1>
+      </div>
+    }
+    else {
+      return <div style={{ padding: '24px' }}>
+        <h1>Marketplace</h1>
+        <Row gutter={[16, 16]}>
+          {products.map((product, index) => (
+            <Col key={index} span={4}>
+              {product.product ? (
+                <Card
+                  hoverable
+                  cover={<img alt="product" src={"http://localhost:5000" + product.product} />}
+                >
+                  <Card.Meta
+                    title={`Product ${index + 1}`}
+                  />
+                  <p>$ {product.price}</p>
+                  <p>{product.userName}</p>
+                </Card>
+              ) : null}
+            </Col>
+          ))}
+          {/* Add an empty card to fill the last row if necessary */}
+          {products.length % 5 !== 0 && <Col span={4} />}
+        </Row>
+      </div>
+    }
+  }
+
+
   return (
     <div style={{ padding: '24px' }}>
-      <h1>Marketplace</h1>
       <Row gutter={[16, 16]}>
         {products.map((product, index) => (
           <Col key={index} span={4}>
-            {product.image ? (
+            {product.product ? (
               <Card
                 hoverable
-                cover={<img alt="product" src={product.image} />}
+                cover={<img alt="product" src={"http://localhost:5000" + product.product} />}
               >
                 <Card.Meta
                   title={`Product ${index + 1}`}
-                  description={`$${product.price}`}
                 />
+                <p>$ {product.listing_price}</p>
+                <p>{product.userName}</p>
               </Card>
             ) : null}
           </Col>
